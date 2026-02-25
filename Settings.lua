7c0bcf5c3546f4d36d9211c44d283d0f9cd232c5
geeet1 <120192641+geeet1@users.noreply.github.com>
2026-02-25 12:10:17 +0100

    Increase border adjust slider max value to 50

--- Settings.lua
+++ Settings.lua
@@ -794,7 +794,7 @@ function CA:PopulateTabContent(content, castbarType)
     
     content.borderAdjustSlider = CreateFrame("Slider", "CA_BorderAdjust_"..castbarType, content, "OptionsSliderTemplate")
     content.borderAdjustSlider:SetPoint("LEFT", content.borderAdjustLabel, "RIGHT", 15, 0)
-    content.borderAdjustSlider:SetMinMaxValues(0, 10)
+    content.borderAdjustSlider:SetMinMaxValues(0, 50)
     content.borderAdjustSlider:SetValueStep(1)
     content.borderAdjustSlider:SetObeyStepOnDrag(true)
     content.borderAdjustSlider:SetWidth(180)
